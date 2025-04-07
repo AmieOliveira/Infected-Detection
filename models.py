2c8be4867af81b953fd43450cea1c67b4080faba
Amanda <amandacno@coppe.ufrj.br>
2025-04-07 18:16:21 -0300

    Continuation of GNN trianing pipeline -- split the dataset, added data loaders, created model -- THERE IS A BUG IN THE TRAINING OF THE ML MODEL (to be fixed)

---
+++ models.py
@@ -0,0 +1,49 @@
+"""
+    File containing the AI models for the project
+"""
+
+import torch
+import torch.nn as nn
+from torch_geometric.data import Data
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch.nn import BCEWithLogitsLoss
+
+
+# TODO: Conferir a classe
+class GCN(torch.nn.Module):
+    # TODO: Documentação
+    def __init__(
+            self, dim_in=128, dim_layer=128, dim_out=1
+    ):
+        super().__init__()
+        self.conv1 = GCNConv(dim_in, dim_layer)
+        self.conv2 = GCNConv(dim_layer, dim_out)    # ultima dimensao vai ser a dimensao de G.H
+
+    def forward(self, data):
+        x = self.conv1(data.x, data.edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = self.conv2(x, data.edge_index)
+        x = F.softmax(x)
+        return x
+
+
+def train(model, loader, optimizer, device,epochs):
+    model.train()
+    total_loss = 0
+    criterion = BCEWithLogitsLoss()
+    for epoch in range(epochs):
+        for data in loader:
+            data = data.to(device)
+            optimizer.zero_grad()
+            out = model(data)  # out.shape: [N, 1]
+
+            # Garante que y está em float e no mesmo shape
+            y = data.y.float().view(-1, 1)
+
+            loss = criterion(out, y)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        print(f'Epoch {epoch+1}/{epochs}, Loss: {total_loss / len(loader)}')
--- treina_gnn.py
+++ treina_gnn.py
@@ -9,6 +9,7 @@ import yaml
 import numpy as np
 import torch
 from formatacao import EpidemicDataset
+from models import GCN, train
 
 
 parser = argparse.ArgumentParser(
@@ -38,7 +39,7 @@ parser.add_argument(
 dat_parser = parser.add_argument_group(
     "Dataset arguments",
     description="Arguments to select the data set to be used. Whatever \n"
-                "arguments gicen will overwrite the corresponding \n"
+                "arguments given will overwrite the corresponding \n"
                 "configuration file inputs.",
 )
 dat_parser.add_argument(
@@ -52,9 +53,25 @@ dat_parser.add_argument(
          "(separated by spaces. Note that the observed infected \n"
          "nodes ('OBS_I') should be always provided as input.",
 )
+dat_parser.add_argument(
+    "--batch-size", "--batch", type=int,
+    help="Batch size parameter."
+)
+
+mod_parser = parser.add_argument_group(
+    "GNN Model arguments",
+    description="Arguments to configure the NN model to be used. Whatever \n"
+                "arguments given will overwrite the corresponding \n"
+                "configuration file inputs.",
+)
+mod_parser.add_argument(
+    "--dim-layer", type=int,
+    help="Dimension of the model hidden layer."
+)
 # TODO: Finish argparse
 
 args = parser.parse_args()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # 1. Read from configuration file
 # TODO: Configuration file
@@ -76,11 +93,57 @@ print(f"Getting data from folder {data_path}.")
 print(f"Model will be trained with the following inputs: {input_fields}")
 # FIXME: Quero colocar hardcoded que o OBS_I deve ser um dos inputs?
 
+#   2.1 Main data set
 dataset = EpidemicDataset(data_path, input_fields)
-# TODO: Separação em treino e teste, criação dos data loaders
 
-# TODO: Criar modelo
-# TODO: Treinar modelo
+#   2.2 Split into train and test sets
+train_dataset, test_dataset = torch.utils.data.random_split(
+    dataset=dataset,
+    lengths=[0.8, 0.2],
+    generator=torch.Generator().manual_seed(int(np.random.random()*100)),
+)
+
+#   2.3 Create train and test data loaders
+batch_size = args.batch_size if args.batch_size else cfg["dataset"]["batch_size"]
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    pin_memory=True,
+    num_workers=2,
+)
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    pin_memory=True,
+    num_workers=2,
+)
+
+# 3. Create GNN model
+input_dims = dataset[0].x.shape[-1]
+dim_layer = args.dim_layer if args.dim_layer else cfg["model"]["dim_layer"]
+
+lrate = 0.01  # TODO: Add parser and read from it and configuration file
+
+model = GCN(
+    dim_in=input_dims,
+    dim_layer=dim_layer,
+    dim_out=1
+).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lrate)
+
+# 4. Train model
+# FIXME: Bug no treinamento
+train(
+    model,
+    train_loader,
+    optimizer,
+    device,
+    1000
+)
+# FIXME: O treino não deveria receber o test_loader tb?
+
+print("Reached here")
+
 # TODO: Salvar modelo
-# TODO: Avaliar modelo??
+# TODO: Avaliar modelo -- e salvar análises no conjunto de treino e de teste
 
