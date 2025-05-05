6395ccfa051e204fc76ac855af2e2735ac2a88aa
Amanda <amandacno@coppe.ufrj.br>
2025-05-05 13:12:41 -0300

    Adicionados graficos com observacao diferente de treino e teste

--- development/graficos.py
+++ development/graficos.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 title = False
+plot_std = False
 
 # Getting data files
 mean_file = "../../Dados/performance/summary_stats_mean.csv"
@@ -17,6 +18,7 @@ base_model_name = "GNN {}, $\\theta = {}$"
 base_data_name = "{} {}k, $\\theta = {}$"
 obs_bet_name = "Obs. Betweenness"
 y_axis_label = "AUC"    # "Average AUC"
+std_str = "_std" if plot_std else ""
 
 
 col_map = {}
@@ -38,46 +40,54 @@ nets = ["BA", "WS"]
 
 
 # 1. Varying obs plots
-fig_size = (8, 6)
+fig_size = (5, 4)       # (6, 4) # usei pra figura 12k... (onde pus só WS)
 transparency = 0.1
 
 obs_values = [0.1, 0.25, 0.50, 0.75, 0.90]
-n = 3
-for net in nets:
-    fig = plt.figure(figsize=fig_size)
+n_vals = [1, 3, 6, 12]
 
-    columns = [base_data_name.format(net, n, o) for o in obs_values]
-    # lines = [base_model_name.format(net, o/100) for o in obs_values] + [obs_bet_name]
-    # df_mean.loc[lines, columns].transpose().plot()
+for n in n_vals:
+    for net in nets:
+        fig = plt.figure(figsize=fig_size)
 
-    for o in obs_values:
-        row = base_model_name.format(net, o)
-        means = df_mean.loc[row, columns]
-        stds = df_std.loc[row, columns]
-        plt.plot(obs_values, means, label=row, marker="^")  # , markeredgecolor="gray"
-        plt.fill_between(obs_values, means + stds, means - stds, alpha=transparency)
-    means = df_mean.loc[obs_bet_name, columns]
-    sdts = df_std.loc[obs_bet_name, columns]
-    plt.plot(obs_values, means, "--", label=obs_bet_name, marker="v")  # , markeredgecolor="gray"
-    plt.fill_between(obs_values, means + stds, means - stds, alpha=transparency)
+        columns = [base_data_name.format(net, n, o) for o in obs_values]
+        # lines = [base_model_name.format(net, o) for o in obs_values] + [obs_bet_name]
+        # df_mean.loc[lines, columns].transpose().to_latex(index=True)
 
-    plt.legend()
-    plt.xticks(obs_values)
-    plt.xlabel("Observation probability $\\theta$")
-    plt.ylabel(y_axis_label)
-    if title:
-        plt.title(f"{net} {n}k nodes")
+        for o in obs_values:
+            row = base_model_name.format(net, o)
+            means = df_mean.loc[row, columns]
+            stds = df_std.loc[row, columns]
+            plt.plot(obs_values, means, label=row, marker="^")  # , markeredgecolor="gray"
+            if std_str:
+                plt.fill_between(obs_values, means + stds, means - stds, alpha=transparency)
+        means = df_mean.loc[obs_bet_name, columns]
+        sdts = df_std.loc[obs_bet_name, columns]
+        plt.plot(obs_values, means, "--", label=obs_bet_name, marker="v")  # , markeredgecolor="gray"
 
-    plt.savefig(f"../Dados/performance/saidas_validacao_{net}_n{n}k_variacao_observacao.pdf",
-                transparent=True)
+        if plot_std:
+            plt.fill_between(obs_values, means + stds, means - stds, alpha=transparency)
+
+        plt.legend()
+        plt.xticks(obs_values)
+        plt.xlabel("Observation probability in the evaluation set ($\\theta_{\\text{eval}}$)")
+        plt.ylabel(y_axis_label)
+        plt.grid(alpha=0.5, linestyle="--")
+        if title:
+            plt.title(f"{net} {n}k nodes")
+        plt.tight_layout()
+
+        plt.savefig(f"../../Dados/performance/saidas_validacao_{net}_n{n}k_variacao_observacao{std_str}.pdf",
+                    transparent=True)
 
 plt.close("all")
+print("Finished first set of images")
 # --------------------------
 
 
 # 2. Varying the network size
-fig_size = (8, 6)
-transparency = 0.1
+fig_size = (7, 3.5)
+# transparency = 0.1
 
 n_values = [1, 3, 6, 12]
 obs_values = [0.1, 0.25, 0.50, 0.75, 0.90]
@@ -88,43 +98,54 @@ colors_obsbet = plt.get_cmap("GnBu")(np.linspace(0.3, 0.95, 5))
 for net in nets:
     fig = plt.figure(figsize=fig_size)
 
-    cIdx = 0
-    for o in obs_values:
+    for cIdx, o in enumerate(obs_values):
         columns = [base_data_name.format(net, n, o) for n in n_values]
 
         r_model = base_model_name.format(net, o)
         m_model = df_mean.loc[r_model, columns]
         s_model = df_std.loc[r_model, columns]
         plt.plot(n_values, m_model, label=r_model, color=colors_models[cIdx], marker="^")
-        plt.fill_between(n_values, m_model + s_model, m_model - s_model, alpha=transparency,
-                         color=colors_models[cIdx], linewidth=0)
-        # cIdx += 1
+        if plot_std:
+            plt.fill_between(n_values, m_model + s_model, m_model - s_model, alpha=transparency,
+                             color=colors_models[cIdx], linewidth=0)
+
+    for cIdx, o in enumerate(obs_values):
+        columns = [base_data_name.format(net, n, o) for n in n_values]
 
         m_o_bet = df_mean.loc[obs_bet_name, columns]
         s_o_bet = df_std.loc[obs_bet_name, columns]
         plt.plot(n_values, m_o_bet, "--", label=f"Obs. Bet. $\\theta={o}$", color=colors_obsbet[cIdx], marker="v")  # , markeredgecolor="gray"
-        plt.fill_between(n_values, m_o_bet + s_o_bet, m_o_bet - s_o_bet,
-                         alpha=transparency, color=colors_obsbet[cIdx], linewidth=0)
-        cIdx += 1
+        # f"Obs. Bet. $\\theta_\\text{{eval}}={o}$"
+        if plot_std:
+            plt.fill_between(n_values, m_o_bet + s_o_bet, m_o_bet - s_o_bet,
+                             alpha=transparency, color=colors_obsbet[cIdx], linewidth=0)
 
-    plt.legend()
+    plt.legend(ncols=2)
     plt.xticks(n_values)
-    plt.xlabel("Number of nodes $n$")
+    plt.xlabel("Number of nodes in the evaluation set ($n_\\text{eval}$)")
     plt.ylabel(y_axis_label)
+    plt.grid(alpha=0.5, linestyle="--")
     if title:
         plt.title(f"{net} data")
+    plt.tight_layout()
 
-    plt.savefig(f"../Dados/performance/saidas_validacao_{net}_variacao_n_multiplos_obs.pdf",
+    plt.savefig(f"../../Dados/performance/saidas_validacao_{net}_variacao_n_multiplos_obs{std_str}.pdf",
                 transparent=True)
 
 plt.close("all")
+print("Finished second set of images")
+# ----------------------------------
 
 
 # 3. Modelo treinado em uma rede, e testado das duas redes (no mesmo gráfico)
-fig_size = (8, 6)
-transparency = 0.1
+fig_size = (5, 4)
+f_size = 5
+# transparency = 0.1
 n_values = [1, 3, 6, 12]
 obs_values = [0.1, 0.25, 0.50, 0.75, 0.90]
+
+marcadores = {"BA": "o", "WS": "x", obs_bet_name: "s"}
+l_style = {"BA": "--", "WS": "-"}
 
 # 3.1 Eixo X é o número de nós
 for o in obs_values:
@@ -137,21 +158,86 @@ for o in obs_values:
 
             m_model = df_mean.loc[r_model, columns]
             s_model = df_std.loc[r_model, columns]
-            plt.plot(n_values, m_model, label=f"Trained in {net}, tested in {net_option}", marker="^")
-            plt.fill_between(n_values, m_model + s_model, m_model - s_model, alpha=transparency)
+            plt.plot(n_values, m_model, l_style[net_option], label=f"Trained in {net}, tested in {net_option}",
+                     marker=marcadores[net])
+            if plot_std:
+                plt.fill_between(n_values, m_model + s_model, m_model - s_model, alpha=transparency)
+
+    for net in nets:
+        columns = [base_data_name.format(net, n, o) for n in n_values]
+        m_o_bet = df_mean.loc[obs_bet_name, columns]
+        s_o_bet = df_std.loc[obs_bet_name, columns]
+        plt.plot(n_values, m_o_bet, l_style[net], label=f"Obs. Bet. in {net} data",
+                 marker=marcadores[obs_bet_name])  # , markeredgecolor="gray"
+        if plot_std:
+            plt.fill_between(n_values, m_o_bet + s_o_bet, m_o_bet - s_o_bet,
+                             alpha=transparency, linewidth=0)
 
         # TODO: obs. betweenness??
 
-    plt.legend()
+    plt.legend(ncols=3, bbox_to_anchor=(0.5, 1.15), loc='upper center', fontsize=f_size)
     plt.xticks(n_values)
-    plt.xlabel("Number of nodes $n$")
+    plt.xlabel("Number of nodes in the evaluation set ($n_\\text{eval}$)")
     plt.ylabel(y_axis_label)
     plt.grid(alpha=0.5, linestyle="--")
     if title:
-        plt.title(f"$\\theta = {o}$")
+        plt.title(f"$\\theta_\\text{{eval}} = {o}$")
+    plt.tight_layout()
 
-    plt.savefig(f"../Dados/performance/saidas_validacao_obs_{int(o*100)}_variacao_n_e_rede.pdf",
+    plt.savefig(f"../../Dados/performance/saidas_validacao_obs_{int(o*100)}_variacao_n_e_rede{std_str}.pdf",
                 transparent=True)
+
+
+print("Finished third set of images")
+
+
+# 3.1.1 Eixo X é o número de nós -- só rede obs 90
+obs = 0.9
+for o in obs_values:
+    if o == obs:
+        continue
+
+    plt.figure(figsize=fig_size)
+
+    for net in nets:
+        r_model = base_model_name.format(net, obs)
+
+        for net_option in nets:
+            columns = [base_data_name.format(net_option, n, o) for n in n_values]
+
+            m_model = df_mean.loc[r_model, columns]
+            s_model = df_std.loc[r_model, columns]
+            plt.plot(n_values, m_model, l_style[net_option], label=f"Trained in {net}, tested in {net_option}",
+                     marker=marcadores[net])
+            if plot_std:
+                plt.fill_between(n_values, m_model + s_model, m_model - s_model, alpha=transparency)
+
+    for net in nets:
+        columns = [base_data_name.format(net, n, o) for n in n_values]
+        m_o_bet = df_mean.loc[obs_bet_name, columns]
+        s_o_bet = df_std.loc[obs_bet_name, columns]
+        plt.plot(n_values, m_o_bet, l_style[net], label=f"Obs. Bet. in {net} data",
+                 marker=marcadores[obs_bet_name])  # , markeredgecolor="gray"
+        if plot_std:
+            plt.fill_between(n_values, m_o_bet + s_o_bet, m_o_bet - s_o_bet,
+                             alpha=transparency, linewidth=0)
+
+        # TODO: obs. betweenness??
+
+    plt.legend(ncols=3, bbox_to_anchor=(0.5, 1.15), loc='upper center', fontsize=f_size)
+    plt.xticks(n_values)
+    plt.xlabel("Number of nodes in the evaluation set ($n_\\text{eval}$)")
+    plt.ylabel(y_axis_label)
+    plt.grid(alpha=0.5, linestyle="--")
+    if title:
+        plt.title(f"$\\theta_\\text{{eval}} = {o}, \\theta = {obs}$")
+    plt.tight_layout()
+
+    plt.savefig(f"../../Dados/performance/saidas_validacao_obs_{int(o*100)}_treinoObs_{obs}_variacao_n_e_rede{std_str}.pdf",
+                transparent=True)
+
+print("Finished fourth set of images")
+
 
 # # 3.2 Eixo X é a taxa de observação
 # for n in n_values:
@@ -160,4 +246,5 @@ for o in obs_values:
 #             r_model = base_model_name.format(net, o)
 
 plt.close("all")
+
 # plt.show()
